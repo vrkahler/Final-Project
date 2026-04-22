9fc7752e4f0e8af7a5c54d29a09d33f90dbafeb5
Victoria Kahler <vrkahler@seu.edu>
2026-04-22 10:49:33 -0400

    Move project from Codespace to local machine

---
+++ app/routes.py
@@ -0,0 +1,32 @@
+from flask import render_template, request, redirect, url_for
+from app import app
+
+tasks = []
+
+@app.route('/')
+@app.route('/dashboard')
+def dashboard():
+    return render_template("dashboard.html", tasks=tasks)
+
+@app.route("/add-task", methods=["GET", "POST"])
+def add_task():
+    if request.method == "POST":
+        task_name = request.form["task_name"]
+        class_name = request.form["class_name"]
+        due_date = request.form["due_date"]
+        tasks.append({
+            "name": task_name,
+            "class": class_name,
+            "date": due_date
+        })
+        print(tasks)
+
+        return redirect(url_for("dashboard"))
+
+    return render_template("add_task.html")
+
+@app.route("/delete-task/<index>")
+def delete_task(index):
+    index = int(index)
+    tasks.pop(index)
+    return redirect(url_for("dashboard"))
